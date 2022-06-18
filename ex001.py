b704e6112374b676d0e44607cb3e986ef4f052ff
Tiago Domingos Figueiredo <33582859+tiagodfigueiredo7@users.noreply.github.com>
2022-06-18 12:17:46 -0300

    novo

--- ex001.py
+++ ex001.py
@@ -3,18 +3,28 @@ print ('ola MUNDO !!, seja bem vindo',nome)
 
 
 
+nome2 = input ('Me fale o nome de uma linda mulher? ')
+print ('Caramba a ' +nome2+ ' é muito linda mesmo Uau ! Concordo com vc!')
+
+cor = input ('Qual a cor favorita dela? ')
+print ('Que legal ela gosta de ' +cor+ ' linda cor')
+
+hoobie = input ('O que ela gosta de fazer? ')
+print ( 'serio que ela gosta de ' +hoobie+ ' que legal eu tbm' )
+
+inf = input ('Vamos perguntar algumas coisas ok?')
+print ('legal vamos lá')
 
 
+dia = input ('qual dia ela nasceu? ')
+mes = input ('qual mes? ')
+ano = input ('qual ano? ')
+print ('legal a ' +nome2+ ' nasceu no ' +dia+ ' de ' +mes+ ' de ' +ano+ ' correto?')
+
+futebol = input ('' +nome+ ' qual seu time favorito? ')
+print ('legal você gosta do', futebol)
 
 
+texto = input ('legal vi que você ' +nome+ ' gosta da ' +nome2+ ' sabemos que ela gosta de algumas coisas como a cor ' +cor+ ' e que gosta de ' +hoobie+ ' certo?')
 
-
-cor = input ('qual sua cor favorita: ')
-print ('minha cor favorita é', cor)
-
-
-futebol = input ('qual seu time de futebol favorito? ')
-print ('que legal vc torce para o', futebol)
-
-jogador = input ('ja que vc torce para', futebol 'qual seu jogador favorito?')
-print = ('que legal, você gosta do jogador', jogador)
+print ('parabens')
--- ex002.py
+++ ex002.py
@@ -1,8 +1,8 @@
 nome = input ('Digite seu nome ')
 print ('É um prazer te conhecer', nome)
 
-Valor1 = input ('coloque o 1 valor ')
-Valor2 = input ('coloque o 2 valor ')
-soma = (int(Valor1)+int(Valor2))
-print ('O resultado é:',soma)
 
+Valor1 = int ( input ('qual o primeiro valor?')) 
+Valor2 = int ( input ('qual o segundo valor ?')) 
+s = Valor1+Valor2 
+print ('a soma dos valores é', s)
