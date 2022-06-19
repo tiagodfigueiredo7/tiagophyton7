7b5e7c540e3201e096633ef387ce1e3faafc24a6
Tiago Domingos Figueiredo <33582859+tiagodfigueiredo7@users.noreply.github.com>
2022-06-18 23:59:39 -0300

    teste

--- desafio008.py
+++ desafio008.py
@@ -1,17 +1,7 @@
-nome = input ('Qual seu nome? ')
-disciplina = input ('' +nome+ ' Qual é a diciplina? ')
-n1 = int ( input (''+ nome + ' qual sua nota nas atividades da diciplina ' +disciplina+ '?'))
-nota = n1
-print ('legal sua nota é', nota)
-n2 = int (input ('' +nome+ ' qual sua nota na da prova da diciplina ' +disciplina+ '?'))
-nota2 =n2
-print ('A sua nota da Prova foi', nota2)
-s = n1+n2
-m = (n1+n2)/2
-print ('Muito bom a soma da suas notas é {}, e sua media é {},' .format(s,m))
-
-texto = input ('computando... computando..Aperte ENTER para descobrir se você passou')
-
-print ('Parabens ' +nome+ ' sua media foi {},' .format(m), end='')
-print (' Você Passou!!!')
-
+metro = int ( input ("quantos metros tem um campo de futebol?"))     
+print ('o campo tem', metro, end='')
+print (' metros')
+print ('esse valor é igual a:')
+c = metro*100
+m = metro*10000
+print ('esse valor é igual a {} centimetros! e igual a {} milimetros!' .format(c,m))
---
+++ desafio009.py
@@ -0,0 +1,46 @@
+tab = int (input ('Vamos brincar de Tabuada, escolha um numero: '))
+print ('Você escolheu o numero:',tab)
+
+print ('Você quer fazer',tab , end='')
+r = int (input(' X quanto?: '))
+t1 = tab*r
+print ('O resultado dessa multiplicação é: {}' .format(t1,))    
+print ('Parabéns, porem vou mostrar a tabuada do numero escolhido completo a baixo')
+m0 = tab*0
+m1 = tab*1
+m2 = tab*2
+m3 = tab*3
+m4 = tab*4
+m5 = tab*5
+m6 = tab*6
+m7 = tab*7
+m8 = tab*8
+m9 = tab*9
+m10 = tab*10
+
+print ('',tab, end ='')
+print (' x 0 é igual: = {}' .format(m0))  
+print ('',tab, end ='')
+print (' x 1 é igual: = {}' .format(m1))  
+print ('',tab, end ='')
+print (' x 2 é igual: = {}' .format(m2))  
+print ('',tab, end ='')
+print (' x 3 é igual: = {}' .format(m3)) 
+print ('',tab, end ='') 
+print (' x 4 é igual: = {}' .format(m4)) 
+print ('',tab, end ='') 
+print (' x 5 é igual: = {}' .format(m5))  
+print ('',tab, end ='')
+print (' x 6 é igual: = {}' .format(m6))  
+print ('',tab, end ='')
+print (' x 7 é igual: = {}' .format(m7))  
+print ('',tab, end ='')
+print (' x 8 é igual: = {}' .format(m8)) 
+print ('',tab, end ='') 
+print (' x 9 é igual: = {}' .format(m9))
+print ('',tab, end ='')
+print (' x 10 é igual: = {}' .format(m10))
+
+
+
+
