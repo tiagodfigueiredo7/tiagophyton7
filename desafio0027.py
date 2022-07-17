096e04b3973773da345204b8295a10565e1a66c4
Tiago Domingos Figueiredo <33582859+tiagodfigueiredo7@users.noreply.github.com>
2022-07-17 12:40:29 -0300

    teste

---
+++ desafio0027.py
@@ -0,0 +1,21 @@
+from tkinter import N
+
+
+n = str(input('Qual seu Nome?')).strip()
+nome =  n.split()
+print ('Seu primeiro nome é {}'.format(nome[0]))
+print ('Seu ultimo nome é {}'.format(nome[len(nome)-1]))
+
+
+
+
+
+
+# esse codigo tras true ou false, se o nome digitado é tem a palavra tiago
+
+
+
+
+
+
+
